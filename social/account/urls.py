f6dcf2925dec1c8e2af36e19b22e3a29e440fb45
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-08-02 14:44:57 +0000

    Adjusted settings.html

--- social/account/urls.py
+++ social/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.base, name='base'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
     path('follow', views.follow, name='follow'),
--- social/account/views.py
+++ social/account/views.py
@@ -10,7 +10,7 @@ import random
 # Create your views here.
 
 @login_required(login_url='signin')
-def index(request):
+def base(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
@@ -54,7 +54,7 @@ def index(request):
     suggestions_username_profile_list = list(chain(*username_profile_list))
 
 
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts':feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
+    return render(request, 'base.html', {'user_profile': user_profile, 'posts':feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
 @login_required(login_url='signin')
 def upload(request):
