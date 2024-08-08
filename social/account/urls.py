547dbbec422947a635712428524bb83502b00f1a
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-08-08 19:52:26 +0000

    Made changes to fix bug

--- social/account/urls.py
+++ social/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
+    path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
     path('follow', views.follow, name='follow'),
