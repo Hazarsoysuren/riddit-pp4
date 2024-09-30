64e0ee29cefc793b18eecb3596c5abc0f469601f
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-09-30 10:19:05 +0000

    Added allowed hosts for heroku

--- social/social/settings.py
+++ social/social/settings.py
@@ -18,7 +18,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-os.environ['DISABLE_COLLECTSTATIC'] = '1'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -27,9 +26,9 @@ os.environ['DISABLE_COLLECTSTATIC'] = '1'
 SECRET_KEY = 'django-insecure-17w30!19g085o^&@&yzm=%%#7y*l*m4x!4-rk3(#f(v$7-5x=u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'.herokuapp.com']
 
 
 # Application definition
