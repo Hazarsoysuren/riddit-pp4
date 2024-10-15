e662c54f1b53f9761010a3f400aead35341190b8
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-10-15 15:36:30 +0000

    Added more wsgi

--- project/social/wsgi.py
+++ project/social/wsgi.py
@@ -6,8 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
+import sys
 import os
+
+# Add the project directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social.settings')
