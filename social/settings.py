e3708f3fdcd901332da41ec4531bfafc91f8551b
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-12-18 19:08:45 +0000

    Changed line on settings.py

--- social/settings.py
+++ social/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 django_heroku.settings(locals())
 
 
